026044d101c03e4fd8ca302219ecd03bcc8a2cd6
arcticlion <david.malan@aol.com>
2014-08-01 11:38:49 +0800

    write a Function setInnerText in dom\ extension.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 10 DOM/dom extension.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 10 DOM/dom extension.js
@@ -32,3 +32,14 @@ function contains(refNode, otherNode) {
 function getInnerText(element) {
     return (typeof element.textContent == "string") ? element.textContent: element.innerText;
 }
+
+function setInnerText(element, text) {
+    if (typeof element.textContent == "string") {
+        element.textContent = text;
+    } else {
+        element.innerText = text;
+    }
+}
+
+setInnerText(div, "Hello world!");
+alert(getInnerText(div));    //"Hello world!"
