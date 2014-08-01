88cc3d1f861393b1bc84a0e9572b2bbcc6853a16
arcticlion <david.malan@aol.com>
2014-08-01 12:25:08 +0800

    add dynamic\ script.js
    Signed-off-by: arcticlion <david.malan@aol.com>

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 10 DOM/dynamic script.js
@@ -0,0 +1,9 @@
+/*
+ * 创建动态脚本
+ * <script type="text/javascript" src="client.js"></script>
+ */
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "client.js";
+document.body.appendChild(script);
+
