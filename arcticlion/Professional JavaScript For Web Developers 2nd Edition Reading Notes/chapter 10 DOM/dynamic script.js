7a74b4b3770c32b9cee2b70b6219e681822439fc
arcticlion <david.malan@aol.com>
2014-08-01 12:31:01 +0800

    refine dynamic\ script.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 10 DOM/dynamic script.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 10 DOM/dynamic script.js
@@ -1,9 +1,13 @@
 /*
  * 创建动态脚本
  * <script type="text/javascript" src="client.js"></script>
+ * 用函数封装
  */
-var script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "client.js";
-document.body.appendChild(script);
+function loadScript(url) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+    document.body.appendChild(script);
+}
 
+loadScript("client.js");
