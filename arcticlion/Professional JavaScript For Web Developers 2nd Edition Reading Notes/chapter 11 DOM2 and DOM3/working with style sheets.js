9fcfb8d228129673f5fe074548fe357611f2e134
arcticlion <david.malan@aol.com>
2014-08-05 16:12:58 +0800

    write a Usage of document.styleSheets[0].cssRules[0] in working\ with\ style\ sheets.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/working with style sheets.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/working with style sheets.js
@@ -22,3 +22,23 @@ function getStyleSheet(element) {
 var link = document.getElementsByTagName("link")[0];
 var sheet = getStyleSheet(link);
 
+/*
+ * 取得与规则相关的样式信息
+ * CSSRule对象表示样式表中的每一条规则
+ * slectorText:返回当前规则的选择符文本
+ * style:可以通过它设置和取得规则中的特定样式值
+ * style.cssText:只包含样式信息
+ * div.box {
+ *     background-color: blue;
+ *     width: 100px;
+ *     height: 200px;
+ * }
+ */
+var sheet = document.stylesheets[0];
+var rules = sheet.cssRules || sheet.rules;    //取得规则列表
+var rule = rules[0];                          //取得第一条规则
+alert(rule.selectorText);                     //"div.box"
+alert(rule.style.cssText);                    //完整的CSS代码
+alert(rule.style.backgroundColor);            //"blue"
+alert(rule.style.width);                      //"100px"
+alert(rule.style.height);                     //"200px"
