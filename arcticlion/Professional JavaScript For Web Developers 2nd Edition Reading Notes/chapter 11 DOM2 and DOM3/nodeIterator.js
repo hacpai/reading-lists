f7abdd0fd628fac1d5e021e7a84718448e9c4b38
arcticlion <david.malan@aol.com>
2014-08-09 23:56:10 +0800

    write other Usage of filter in nodeIterator.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/nodeIterator.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/nodeIterator.js
@@ -10,6 +10,7 @@
  * 可以使用按位或操作符组合，譬如var whatToShow = NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT
  * filter是一个只接收参数为节点的acceptNode()方法的对象
  * 也可以是一个与acceptNode()方法类似的函数
+ * 不指定过滤器，传入null
  * acceptNode()方法对于应该访问的节点，返回NodeFilter.FILTER_ACCEPT;不应该访问的节点，返回NodeFilter.FILTER_SKIP
  * 下列代码展示创建一个只显示<p>元素的节点迭代器
  */
@@ -19,5 +20,11 @@ var filter = {
     }
 };
 var iterator = document.createNodeIterator(root, NodeFilter.SHOW_ELEMENT, filter, false);
+//第三个参数也可以是一个与acceptNode()方法类似的函数
+var filter = function(node) {
+    return node.tagName.toLowerCase() == "p" ? NodeFilter.FILTER_ACCEPT: NodeFilter.FILTER_SKIP;
+}
 
+//能够访问所有类型节点
+var iterator = document.createNodeIterator(root, NodeFilter.SHOW_ALL, null, false);
 
