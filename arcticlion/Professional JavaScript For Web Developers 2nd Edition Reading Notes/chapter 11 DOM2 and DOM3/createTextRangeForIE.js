c2bad548eacd543c1a28a4b1c37f699ef39cc34e
arcticlion <david.malan@aol.com>
2014-08-11 02:39:51 +0800

    write a Usage findText() in createTextRangeForIE.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/createTextRangeForIE.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 11 DOM2 and DOM3/createTextRangeForIE.js
@@ -3,9 +3,12 @@
  * var range = document.body.createTextRange()
  * findText()简单实现选择
  * <p id="p1"><b>Hello</b> world!</p>
+ * 接受一个参数：正值,当前位置向后搜索;负值，反之
  */
 var range = document.body.createTextRange();
 var found = range.findText("Hello");    //选择“Hello”
 alert(found);    //true
 alert(range.text);    //"Hello"
+
+var foundAgain = range.findText("Hello", 1);
 
