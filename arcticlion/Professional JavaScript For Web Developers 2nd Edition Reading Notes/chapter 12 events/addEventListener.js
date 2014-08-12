3c9f5f6b1047aeeb3b37ef74fa925712b5bbd660
arcticlion <david.malan@aol.com>
2014-08-12 23:36:21 +0800

    write a Function of addEventListener in addEventListener.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/addEventListener.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/addEventListener.js
@@ -4,9 +4,16 @@
  * 要处理的事件名
  * 作为事件处理程序的函数
  * 是否捕获阶段调用事件处理程序
+ * 添加的事件处理程序也是在其依附的元素作用域中运行
+ * 两个事件处理程序会按照添加它们的顺序触发
  */
 var btn = document.getElementById("myBtn");
 btn.addEventListener("click", function() {
-    alert(this.id);
+    alert(this.id);    //"myBtn"
 }, false);    //false指冒泡阶段
+btn.addEventListener("click", function() {
+    alert("Hello, world!");
+}, false);
+
+
 
