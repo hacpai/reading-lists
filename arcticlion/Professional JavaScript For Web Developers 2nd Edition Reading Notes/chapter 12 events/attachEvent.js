2e3e6726218537a9691b3450ec088a180ebd379b
arcticlion <david.malan@aol.com>
2014-08-13 00:32:33 +0800

    attachEvent按添加的相反顺序触发
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/attachEvent.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/attachEvent.js
@@ -5,10 +5,16 @@
  * 事件程序名称:需要加"on"
  * 事件处理函数
  * 事件处理程序的作用域为window
+ * 添加多个事件处理程序以相反的添加顺序触发
  */
 var btn = document.getElementById("myBtn");
 btn.attchEvent("onclick", function() {
     alert("Click");
     alert(this === window);    //true
 });
+btn.attachEvent("onclick", function() {
+    alert("Hello world!");
+});    //"Hello world!"
+         "Click"
+         true
 
