c5703eb005d2248af0fbe4d5efae95e10b8300e5
arcticlion <david.malan@aol.com>
2014-08-13 01:01:41 +0800

    add addHandler.js
    Signed-off-by: arcticlion <david.malan@aol.com>

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/addHandler.js
@@ -0,0 +1,26 @@
+/*
+ * 以跨浏览器方式处理事件
+ * 一般使用js库
+ * EventUtil对象处理浏览器之间的差异
+ * EventUtil对象中有一个addHandler()
+ * 接受3个参数
+ * 要操作的元素
+ * 事件名称
+ * 事件处理函数
+ *     if addEventListener添加事件处理程序
+ *     elif attachEvent
+ *     else DOM0级方法
+ */
+var EventUtil = {
+
+    addHandler: function(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    }
+}
+
