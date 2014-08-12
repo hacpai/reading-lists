54f16623535089f96ef335faa8f693e31d0ede37
arcticlion <david.malan@aol.com>
2014-08-12 23:20:07 +0800

    证明DOM0级事件处理程序是元素的方法
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/onclick.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/onclick.js
@@ -1,9 +1,13 @@
 /*
  * 事件处理程序名称以"on"
  * 因此"click"事情处理程序就是"onclick"
+ * DOM0级事件处理程序
+ * 单击按钮显示的是元素的ID
+ * 元素的方法
  */
 var btn = document.getElementById("myBtn");
 btn.onClick = function() {
     alert("CLicked");
+    alert(this.id);    //"myBtn"
 }
 
