3d20092caee07962971c24ad2c07fe5841252711
arcticlion <david.malan@aol.com>
2014-08-13 00:36:21 +0800

    write a Method of detachEvent in attachEvent.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/attachEvent.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/attachEvent.js
@@ -6,6 +6,7 @@
  * 事件处理函数
  * 事件处理程序的作用域为window
  * 添加多个事件处理程序以相反的添加顺序触发
+ * detachEvent()移除事件处理函数
  */
 var btn = document.getElementById("myBtn");
 btn.attchEvent("onclick", function() {
@@ -17,4 +18,11 @@ btn.attachEvent("onclick", function() {
 });    //"Hello world!"
          "Click"
          true
+
+var handler = function() {
+    alert("Click");
+};
+btn.attachEvent("onclick", handler);
+
+btn.detachEvent("onclick", handler);
 
