20b00d37861f131de819ea96984ec4009458d47c
arcticlion <david.malan@aol.com>
2014-08-13 01:07:46 +0800

    add removeHandler() in addHandler.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/addHandler.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/addHandler.js
@@ -21,6 +21,16 @@ var EventUtil = {
         } else {
             element["on" + type] = handler;
         }
+    },
+
+    removeHandler: function(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type = null];
+        }
     }
 }
 
