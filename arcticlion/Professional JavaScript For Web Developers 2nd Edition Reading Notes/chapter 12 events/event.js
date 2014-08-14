bed484f36d4a6269a2a0ca5771ee82f481850e80
arcticlion <david.malan@aol.com>
2014-08-14 23:14:06 +0800

    add event.js
    Signed-off-by: arcticlion <david.malan@aol.com>

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/event.js
@@ -0,0 +1,14 @@
+/*
+ * 在触发DOM上某个事件时
+ * 会自动产生event对象
+ * 包含事件元素、类型、及特定事件相关信息
+ * event,type表示事件类型
+ */
+var btn = document.getElementById("myBtn");
+btn.onclick = function(event) {
+    alert(event.type);    //"click"
+};
+btn.addEventListener("click", function(event) {
+    alert(event.type);    //"click"
+}, false);
+
