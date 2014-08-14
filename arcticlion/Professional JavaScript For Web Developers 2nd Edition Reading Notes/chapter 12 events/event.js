a5f5a077ee26e5598966289868efa2627a71d8fa
arcticlion <david.malan@aol.com>
2014-08-14 23:38:21 +0800

    write a Usage of event.currentTarget&event.target in event.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/event.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/event.js
@@ -3,10 +3,15 @@
  * 会自动产生event对象
  * 包含事件元素、类型、及特定事件相关信息
  * event,type表示事件类型
+ * 事件处理程序内部this === event.currentTarget
+ * event.target等于真正的目标值
+ * 直接将事件处理程序指点给目标元素，三者相等
  */
 var btn = document.getElementById("myBtn");
 btn.onclick = function(event) {
     alert(event.type);    //"click"
+    alert(event.currentTarget === this);    //true
+    alert(event.target === this);    //true
 };
 btn.addEventListener("click", function(event) {
     alert(event.type);    //"click"
