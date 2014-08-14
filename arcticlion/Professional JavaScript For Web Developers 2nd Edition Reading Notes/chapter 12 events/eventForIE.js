b7ea91e3d2ddc95ee118618b35de0845b6d60175
arcticlion <david.malan@aol.com>
2014-08-15 00:51:47 +0800

    attachEvent()中event的使用
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/eventForIE.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/eventForIE.js
@@ -2,10 +2,16 @@
  * IE的event对象访问方式取决于指定事件处理程序的方法
  * DOM0级方法添加事件处理程序
  * event对象作为window对象一个属性
+ * attachEvent()添加事件处理程序
+ * event对象作为参数传入事件处理程序函数中
  */
 var btn = document.getElementById("myBtn");
 btn.onclick = function() {
     var event = window.event;
     alert(event.type);    //"click"
 };
+
+btn.attachEvent("onclick", function(event) {
+    alert(event.type);    //"clcik"
+});
 
