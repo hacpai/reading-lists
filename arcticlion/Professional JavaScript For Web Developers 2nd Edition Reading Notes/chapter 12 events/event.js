c3575a938322a1fc8285d7772031779dd86f6205
arcticlion <david.malan@aol.com>
2014-08-15 00:05:11 +0800

    write a Usage of event.type in event.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/event.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/event.js
@@ -7,6 +7,8 @@
  * event.target等于真正的目标值
  * 直接将事件处理程序指点给目标元素，三者相等
  * 事件处理程序在按钮的富姐电中this === currentTarget
+ * 通过一个函数处理多个事件用event.type
+ * 
  */
 var btn = document.getElementById("myBtn");
 btn.onclick = function(event) {
@@ -22,6 +24,26 @@ btn.addEventListener("click", function(event) {
 document.body.onclick = function(event) {
     alert(event.currentTarget === document.body);    //true
     alert(this === document.body);    //true
-    alert(event.target === document.getElementById("myBtn");    //true
+    alert(event.target === document.getElementById("myBtn");    //true,由于按钮上没有注册事件处理程序，click事件就冒泡到了document.body那里进行处理
 };
+
+var handler = fucnction(event) {
+    switch(event.type) {
+        case "click": 
+            alert("CLicked");
+            break;
+
+        case "mouseover":
+            event.target.style.backgroundColor = "red";
+            break;
+
+        case "mouseout":
+            event.target.style.backgroundColor = "";
+            break;
+    }
+};
+
+btn.onclick = handler;
+btn.onmouseover = handler;
+btn.onmouseout = handler;
 
