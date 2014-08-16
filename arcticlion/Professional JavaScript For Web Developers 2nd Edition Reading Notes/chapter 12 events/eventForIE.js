c37bfed435778551d39d3350c39897dfa05cc392
arcticlion <david.malan@aol.com>
2014-08-16 23:35:14 +0800

    write a Usage of returnValue in eventForIE.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/eventForIE.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/eventForIE.js
@@ -8,6 +8,7 @@
  * IE事件处理程序作用域是根据他指定的方式确定的
  * 故this不一定会等于事件的目标，使用event.srcElement保险
  * DOM0级方法指定的事件处理程序中，srcElement属性===this
+ * returnValue相当于preventDefault()取消给定事件的默认行为
  */
 var btn = document.getElementById("myBtn");
 btn.onclick = function() {
@@ -22,4 +23,10 @@ btn.attachEvent("onclick", function(event) {
 });
 
 <input type="button" value="Click Me" onclick="alert(event.type)"/>
+
+//使用returnValue达到阻止链接默认行为的目的
+var link = document.getElementById("myLink");
+link.onclick = function() {
+    window.event.returnValue = false;
+};
 
