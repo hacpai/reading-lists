15c16ff7ba06c839580d44ad1d9d42f00d8547bb
arcticlion <david.malan@aol.com>
2014-08-18 23:00:36 +0800

    write a Usage of shiftKey,ctrlKey... in mouse.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/mouse.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/mouse.js
@@ -4,11 +4,29 @@
  * clientX和clientY:事件发生时鼠标指针在视口中的水平和垂直坐标
  * 屏幕坐标位置
  * screenX和screenY确定事件发生时鼠标指针相对于整个屏幕的坐标信息
+ * DOM规定4个属性(shiftKey, ctrlKey, altKey, metaKey)修改键的状态
+ * 4个属性返回布尔值：用户按下该键则值为true
  */
 var div = document.getElementById("myDiv");
 EventUtil.addHandler(div, "click", function(event) {
     event = EventUtil.getEvent(event);
     alert("Client coordinates: " + event.clientX + "," + event.clientY);
     alert("Screen coordinates: " + event.screenX + "," + event.screenY);
+    //检测用户按下了修改键数目
+    var keys = new Array();
+
+    if (event.shiftKey) {
+        keys.push("shift");
+
+    if (event.ctrlKey) {
+        keys.push("ctrl");
+
+    if (event.altKey) {
+        keys.push("alt");
+
+    if (event.metaKey) {
+        keys.push("meta");
+
+    alert("Keys: " + keys.join(","));
 });
 
