42ee06c0ef20a17873d248bfe99ae385fa06387e
arcticlion <david.malan@aol.com>
2014-08-18 22:50:39 +0800

    write a Usage of screenX&screenY in mouse.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/mouse.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/mouse.js
@@ -2,10 +2,13 @@
  * 鼠标事件
  * 取得鼠标事件客户端的坐标信息
  * clientX和clientY:事件发生时鼠标指针在视口中的水平和垂直坐标
+ * 屏幕坐标位置
+ * screenX和screenY确定事件发生时鼠标指针相对于整个屏幕的坐标信息
  */
 var div = document.getElementById("myDiv");
 EventUtil.addHandler(div, "click", function(event) {
     event = EventUtil.getEvent(event);
     alert("Client coordinates: " + event.clientX + "," + event.clientY);
+    alert("Screen coordinates: " + event.screenX + "," + event.screenY);
 });
 
