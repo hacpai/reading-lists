c91be4bf4458cd07c2f94cac65dfe2caba0ee17f
arcticlion <david.malan@aol.com>
2014-08-21 23:54:46 +0800

    write a Usage of event.data in keybroad.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/keybroad.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/keybroad.js
@@ -1,11 +1,20 @@
 /*
  * 键盘事件
- * keyCode:按下字符的ASCII
- * 提窗键码值
+ *     keyCode:按下字符的ASCII
+ *         提窗键码值
+ *     textInput事件
+ *         只有在编辑区域触发
+ *         只有按下实际字符键才触发
+ *         event.data:表示用户输入的字符
  */
 var textbox = document.getElementById("myText");
 EventUtil.addHandler(textbox, "keyup", function(event) {
     event = EventUtil.getEvent(event);
     alert(event.keyCode);
+});
+
+EventUtil.addHandler(textbox, "textInput", function(event) {
+    event = EventUtil.getEvent(event);
+    alert(event.data);
 });
 
