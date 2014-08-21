7539a3c2f72bdf8e63afd48d8fe7c5cb9076dc5c
arcticlion <david.malan@aol.com>
2014-08-22 02:00:50 +0800

    write a HTML way of onload event in htmlEvent.js
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/htmlEvent.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/htmlEvent.js
@@ -2,10 +2,20 @@
  * HTML事件
  *     load事件
  *         当页面加载后触发window上的load事件
+ *         两种定义onload事件处理程序方式
  */
 
 //JavaScript指定事件处理程序方式
 EventUtil.addHandler(window, "load", function(event) {
     alert("Loaded!");
 });
+
+//<body>添加一个onload特性
+<html>
+<head>
+    <title>Load Event Example</title>
+</head>
+<body onload="alert('Loaded')">
+</body>
+</html>
 
