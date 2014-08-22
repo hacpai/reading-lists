4445523bd501c0c8c5eeedacc240ffe9aec571f3
arcticlion <david.malan@aol.com>
2014-08-22 13:09:57 +0800

    添加JavaScript方式触发图像load事件
    Signed-off-by: arcticlion <david.malan@aol.com>

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/htmlEvent.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/htmlEvent.js
@@ -4,7 +4,7 @@
  *         当页面加载后触发window上的load事件
  *         两种定义onload事件处理程序方式
  *         建议使用JavaScript方式
- *         图像也可触发load事件
+ *         图像也有两种可触发load事件
  */
 
 //JavaScript指定事件处理程序方式
@@ -22,4 +22,11 @@ EventUtil.addHandler(window, "load", function(event) {
 </html>
 
 <img src="smile.gif" onload="alert('Image loaded.')" />
+
+var image = document.createElement("img");
+EventUtil.addHandler(image, "load", function(event) {
+    event = EventUtil.getEvent(event);
+    alert(EventUtil.getTarget(event).src);
+});
+
 
