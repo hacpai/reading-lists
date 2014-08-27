371874db163d86f8fd55cb322b3cc5862ed5e464
arcticlion <david.malan@aol.com>
2014-08-28 01:26:48 +0800

    write simulat event in simlatEvent.js

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 12 events/simulatEvent.js
@@ -0,0 +1,61 @@
+/*
+ * 模拟事件
+ *     createEvent()创建event对象，接受创建事件类型的字符串
+ *     dispatchEvent():触发事件
+ *     模拟鼠标事件
+ *         createEvent("MouseEvents")
+ *         initMouseEvent():指定鼠标事件的有关信息,接受15个参数
+ *     模拟键盘事件
+ *         createEvent(KeyEvents)
+ *         initKeyEvent()接受10个参数
+ *     模拟变动事件
+ *         createevent("MutationEvents")
+ *         initMutationEvent()
+ *     模拟HTML事件
+ *         createevent("HTMLEvent")
+ *         initEvent()初始化
+ */
+
+var btn = document.getElementById("myBtn");
+//创建事件对象
+var event = document.createEvent"MouseEvents");
+//初始化事件对象
+event.initMouseEvent("click", true, ture, document.defaultView, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+//触发事件
+btn.dispatchEvent(event);
+
+//只适用于Firefox
+var textbox = document.getElementById("myTextbox");
+//创建事件对象
+var event = document.createEvent("KeyEvents");
+//初始化事件对象
+event.initKeyEvent("keypress", true, true, document.defaultView, false, false, false, false, 65, 65);
+//触发事件
+textbox.dispatchEvent(event);
+
+//创建通用事件
+var textbox = document.getElementById("myTextbox");
+//创建事件对象
+var event = document.createEvent("Events");
+//初始化事件对象
+event.initEvent(type, bubbles, cancelable);
+event.view = document.defaultView;
+event.altKey = false;
+event.ctrlKey = false;
+event.shiftKey = false;
+event.metaKey = false;
+event.keyCode = 65;
+event.charCode = 65;
+//触发事件
+textbox.dispatchEvent(event);
+
+//模拟DOMNodeInserted事件
+var event = document.createEvent("MutationEvents");
+event.initMouseEvent("DOMNodeInserted", true, false, someNode, "", "", "", 0);
+target.dispatchEvent(event);
+
+//给定目标模拟focus事件
+var event = document.createEvent("HTMLEvents");
+event.initEvent("focus", true, false);
+target.dispatchEvent(event);
+
