262b7b0b5339b745e8e9ff9d52c05fd7a4400894
arcticlion <david.malan@aol.com>
2014-08-29 09:31:53 +0800

    write a Usage of selevt() in scriptingTextBoxes.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/scriptingTextBoxes.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/scriptingTextBoxes.js
@@ -6,6 +6,9 @@
  *         <textarea>:多行文本
  *             <textarea rows="25" cols="5">initial value</textarea>
  *         两种方式都会将用户输入保存在value中,可以设置读取何设置文本框的值
+ *     选择文本
+ *         select():不接受参数,调用时将焦点设置到文本框
+ *             
  *
  */
 
@@ -15,4 +18,13 @@ alert(textbox.value);
 
 textbox.value = "Some new value";
 
- 
+textbox.select();
+
+//只要文本框获得焦点就选择其中所有文本,便于删除文本框内的默认文本
+EventUtil.addHandler(textbox, "focus", function(event) {
+    event = EventUtil.getEvent(event);
+    var target = EventUtil.getTarget(event);
+
+    target.select();
+});
+
