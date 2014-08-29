b988aeb39c0cb2b90f3cab4acc9747a9b087a702
arcticlion <david.malan@aol.com>
2014-08-29 14:22:50 +0800

    write a Feature of blocking number charcters in inputFilter.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
@@ -3,9 +3,15 @@
  *     屏蔽字符
  */
 
-//屏蔽所有字符
+//屏蔽所有数字字符
 EventUtil.addHandler(textbox, "keypress", function(event) {
     event = EventUtil.getEvent(event);
-    EventUtil.preventDefault(event);
+    var target = EventUtil.getTarget(event);
+    //跨浏览器取得字符编码
+    var charCode = EventUtil.getCode(event);
+    //再将字符转化成字符串用正则测试
+    if (!/\d/.test(String.fromCharCode(charCode))) {
+        EventUtil.preventDefault(event);
+    }
 });
 
