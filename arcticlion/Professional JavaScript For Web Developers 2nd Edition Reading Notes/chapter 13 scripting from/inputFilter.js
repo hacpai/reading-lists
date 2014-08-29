b933238b7dba3746026bbba75221407614e54657
arcticlion <david.malan@aol.com>
2014-08-29 14:38:39 +0800

    refine a Feature of blocking number charcters in inputFilter.js again

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
@@ -12,7 +12,8 @@ EventUtil.addHandler(textbox, "keypress", function(event) {
     //再将字符转化成字符串用正则测试
     //取消屏蔽方向键和删除键
         //Firefox这些键字符编码为0，Safari3-为8，故屏蔽charCode > 9的字符
-    if (!/\d/.test(String.fromCharCode(charCode)) && charCode > 9) {
+        //取消屏蔽Ctrl
+    if (!/\d/.test(String.fromCharCode(charCode)) && charCode > 9 && !event.CtrlKey) {
         EventUtil.preventDefault(event);
     }
 });
