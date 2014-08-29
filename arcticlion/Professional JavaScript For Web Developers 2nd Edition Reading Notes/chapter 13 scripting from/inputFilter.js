8b1c0510e22e3717ac7fa05f64ac93aa09c87694
arcticlion <david.malan@aol.com>
2014-08-29 16:10:42 +0800

    write a Feature of the text of clipboard is number in inputFilter.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/inputFilter.js
@@ -1,6 +1,8 @@
 /*
  * 过滤输入
  *     屏蔽字符
+ *     操作剪贴板
+ *     
  */
 
 //屏蔽所有数字字符
@@ -14,6 +16,16 @@ EventUtil.addHandler(textbox, "keypress", function(event) {
         //Firefox这些键字符编码为0，Safari3-为8，故屏蔽charCode > 9的字符
         //取消屏蔽Ctrl
     if (!/\d/.test(String.fromCharCode(charCode)) && charCode > 9 && !event.CtrlKey) {
+        EventUtil.preventDefault(event);
+    }
+});
+
+//检测黏贴的值是否为数值
+EventUtil.addHandler(textbox, "paste", function(event) {
+    event = EventUtil.getEvent(event);
+    var text = EventUtil.getClipboardText(event);
+
+    if (!/^\d*$/.test(text)) {
         EventUtil.preventDefault(event);
     }
 });
