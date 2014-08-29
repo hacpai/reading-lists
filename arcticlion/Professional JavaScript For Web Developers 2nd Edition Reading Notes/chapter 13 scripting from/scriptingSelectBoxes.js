490f2bd32ad71ad1e2872140862cac18aa52eadb
arcticlion <david.malan@aol.com>
2014-08-29 21:28:08 +0800

    write a Feature of display all of the information about the selected option in scriptingTextboxes.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/scriptingSelectBoxes.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/scriptingSelectBoxes.js
@@ -1,5 +1,12 @@
 /*
  * 选择框脚本
+ *     选择选项
+ *         selectIndex:访问选中项的最简单方式
+ *         options:控制所有<option>的HTMLCollection
+ *         add(newOption, relOption):<relOption>前插入<newoption>
+ *         multiple:布尔值,是否允许多项选择
+ *         SelectedIndex:选中项索引
+ *
  *     <select>和<option>创建
  * <select name="location" id="selLocation">
  *     <option value="Sunyvalu, CA">Sunnyvale</option>
@@ -12,6 +19,12 @@
 
 var selectbox = document.forms[0].location;
 
-var text = selectbox.option[0].text;
-var value = selectbox.option[0].value;
+var text = selectbox.options[0].text;
+var value = selectbox.options[0].value;
+
+var selectedOption = selectbox.options[selectbox.selectedIndex];
+
+var selectedIndex = selectbox.selectedIndex;
+var selectedOption = selectbox.optons[selectedIndex];
+alert("Selected index: " + selectedIndex + "\nSelected text: " + selectedOption.text + "\nSelected value: " + selectedOption.value);
 
