d352298a2f70705fa23ef2ca77813a9b6b3b0193
arcticlion <david.malan@aol.com>
2014-08-30 20:07:58 +0800

    write a error type in errorHandling.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 14 error handling and debug/errorHandling.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 14 error handling and debug/errorHandling.js
@@ -6,8 +6,17 @@
  *         } catch(error) {
  *             //在错误发生时怎么处理
  *         }
-           finally子句
-               finally一定执行
+ *         finally子句
+ *             finally一定执行
+ *         错误类型
+ *             Error:浏览器抛出和开发人员自定义错误
+ *             EvalError:eval使用方式与定义不相符
+ *             RangeError:数值不在相应的范围时触发
+ *             ReferenceError:访问不存在的变量时触发
+ *             SyntaxError:错误语法传入eval()
+ *             TypeError:变量保存意外类型或访问不存在方法
+ *             URIError:URL格式不正确时
+ *             利用不同的错误类型,可以获悉更多异常信息，有助于对错误做出恰当处理
  */
 
 try {
@@ -28,4 +37,27 @@ function testFinally() {
     }
 }
 
+var item1 = new Array(-20);    //抛出RangeError
+var item2 = new Array(Number.MAX_VALUE);    //抛出RangeError
+
+var obj = x;     //在x并未声明的情况下抛出ReferenceError
+
+eval("a ++ n");    //抛出SyntaxError
+
+var o = new 10;    //抛出TyptError
+alert("name" in true);    //抛出TypeError
+Function.prototype.toString(.call("name");    //抛出TypeError
+
+//知道各种错误类型
+try {
+    someFunction();
+} catch (error) {
+    if (error instanceof TyptError) {
+        //处理类型错误
+    } else if (error instanceof ReferenceError) {
+        //处理引用错误
+    } else {
+        //处理其他类型的错误
+    }
+}
 
