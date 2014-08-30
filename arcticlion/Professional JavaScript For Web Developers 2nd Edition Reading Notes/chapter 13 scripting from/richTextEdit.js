1c403efa84c0596fbe880a150c19431bbf2a59d6
arcticlion <david.malan@aol.com>
2014-08-30 11:40:52 +0800

    set designMode on with onload event handler in richTextEdit.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/richTextEdit.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/richTextEdit.js
@@ -3,6 +3,7 @@
  *     嵌入包含空HTML的iframe,设置designMode使页面可编辑
  */
 
+//简单HTML页面作为内容来源
 <html>
     <head>
         <title>Blank Page for Rich Text Editing</title>
@@ -10,4 +11,13 @@
     <body>
     </body>
 </html>
+
+<iframe name = "richedit" style = "height:100px; width:100px;" src = "black.htm"></iframe>
+
+<script type = "text/javascritpt">
+
+//使用onload事件处理程序设置designMode为on
+EventUtil.addHandler(window, "load", function() {
+    frames["richedit"].document.designMode = "on";
+});
 
