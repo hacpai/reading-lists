b3f41a1d87b86707464a6cf6c330c9c6bd3fd654
arcticlion <david.malan@aol.com>
2014-08-31 01:28:32 +0800

    refine a Function of divide in debug.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 14 error handling and debug/debug.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 14 error handling and debug/debug.js
@@ -74,6 +74,9 @@ function log(message) {
 }
 
 function divide(num1, num2) {
+    if (typeof num1 != "number" || typeof num2 != "number") {
+        throw new Error("divide(): Both arguments must be number.");
+    }
     return num1 / num2;
 }
 
