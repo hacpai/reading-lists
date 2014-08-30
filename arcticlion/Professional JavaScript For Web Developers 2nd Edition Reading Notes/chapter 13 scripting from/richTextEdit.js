c9a1970f983c7b3b1c62885e6e5570896369bfd3
arcticlion <david.malan@aol.com>
2014-08-30 11:34:36 +0800

    write a blank page for rich text editing in richTextEdit.js

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/richTextEdit.js
@@ -0,0 +1,13 @@
+/*
+ * 富文本编辑
+ *     嵌入包含空HTML的iframe,设置designMode使页面可编辑
+ */
+
+<html>
+    <head>
+        <title>Blank Page for Rich Text Editing</title>
+    </head>
+    <body>
+    </body>
+</html>
+
