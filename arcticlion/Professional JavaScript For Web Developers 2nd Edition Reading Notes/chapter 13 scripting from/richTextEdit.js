0c5b0d88ec75463ea8e2f62a96f191405d6a6ec8
arcticlion <david.malan@aol.com>
2014-08-30 12:11:45 +0800

    write a Usage of execCommand in richTextEdit.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/richTextEdit.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 13 scripting from/richTextEdit.js
@@ -1,6 +1,8 @@
 /*
  * 富文本编辑
  *     嵌入包含空HTML的iframe,设置designMode使页面可编辑
+ *     操作富文本
+ *         document.execCommand():3个参数,命令名称,是否为命令提供用户界面,执行命令需要的一个值
  */
 
 //简单HTML页面作为内容来源
@@ -20,4 +22,14 @@
 EventUtil.addHandler(window, "load", function() {
     frames["richedit"].document.designMode = "on";
 });
+
+//使用这些命令修改富文本区域外观
+//转换粗体文本
+frames["richedit"].document.execCommand("bold", false, null);
+//转换斜体文本
+frames["richedit"].document.execCommand("italic", false, null);
+//创建指向www.wrox.com链接
+frames["richedit"].document.execCommand("createlink", false, "http://www.wrox.com");
+//格式化为1级标题
+frames["richedit"].document.execCommand("formatblock", false, "<h1>");
 
