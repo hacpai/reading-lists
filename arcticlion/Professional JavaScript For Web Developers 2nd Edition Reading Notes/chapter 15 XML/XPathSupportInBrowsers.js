5e3879d2e892806421d17aa27fd6a1d2bb9a1c7e
arcticlion <david.malan@aol.com>
2014-09-01 10:49:27 +0800

    write a Usage of snapshotItem() & snapshotLength in XPathSupportInBrowsers.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 15 XML/XPathSupportInBrowsers.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 15 XML/XPathSupportInBrowsers.js
@@ -7,13 +7,15 @@
  *             上下文节点
  *             命名空间求解器
  *             返回结果的类型
+ *                 ORDERED_NODE_ITERATOR_TYPE:返回匹配的节点集合，节点持续于文档中的次序一致，最常见的结果类型。
+ *                 ORDERED_NODE_SPAPSHOT_TYPE:返回节点集合快照
  *             保存结果的XPathResult对象(通常为null)
  *
  */
 
 var supportsXPath = document.implementation.hasFeature("XPath", "3.0");
 
-var result = xmldom.evaluate("employee/name", xmldom.documentElement, null, XPathResult.ORDERED_NODE_ITERATOR_TYPEk, null);
+var result = xmldom.evaluate("employee/name", xmldom.documentElement, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
 
 if (result != null) {
     //节点是一个节点迭代器,iterateNext取得匹配节点
@@ -21,6 +23,15 @@ if (result != null) {
     while(node) {
         alert(node.tagName);
         node = node.iterateNext();
+    }
+}
+
+//快照结果类型必须使用snapshotItem()和snapshotLength
+var result = xmldom.evaluate("employee/name", xmldom.documentElement, null, XPathResult.ORDEEED_NODE_SNAPSHOT_TYPE, null);
+
+if (result !== null) {
+    for (var i = 0, len = result.snapshotLength; i < len; i++) {
+        alert(result.snapshotItem(i).tagName);
     }
 }
 
