5258c2f088b7e325269d4b1da3a64bb300bcca69
arcticlion <david.malan@aol.com>
2014-09-02 17:12:06 +0800

    create Namespace Object in E4XTypes.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 16 E4X/E4XTypes.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 16 E4X/E4XTypes.js
@@ -12,6 +12,7 @@
  *         XML对象的有序集合
  *         通常是在解析较大的XML结构的过程中捎带着被创建的
  *     Namespace：命名空间的前缀与空间URI之间的映射
+ *         Namespace对象表现命名空间
  *     QName：内部名称和命名空间URI组成的一个限定名
  */
 
@@ -60,4 +61,15 @@ alert(employees.employee.length());    //2
 //一个XML对象和只包含一个XML对象的XMLList是没有太大区别
 alert(employees.length());    //1
 alert(employees[0] === employees);    //true
+
+//命名空间的创建
+var ns = new Namespace();
+var ns = new Namespace("http://www.wrox.com/");    //没有前缀的命名空间
+var wrox = new Namespace("wrox", "http://www.wrox.com/");    //wrox命名空间
+//XML字面量包含命名空间时自动创建Namespace对象
+var xml = <wrox:root xmlns:wrox="http://www.wrox.com/">
+            <wrox:message>Hello World!</wrox:message>
+          </wrox:root>;
+//namespace取得对命名空间的引用
+var wrox = xml.Namespace("wrox");
 
