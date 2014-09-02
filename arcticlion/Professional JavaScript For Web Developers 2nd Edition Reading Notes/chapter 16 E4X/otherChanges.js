b7e8c3e43dd8f87d48a33a27115447285aa828ab
arcticlion <david.malan@aol.com>
2014-09-02 23:30:45 +0800

    write a Usage of typeof  in otherChanges.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 16 E4X/otherChanges.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 16 E4X/otherChanges.js
@@ -2,6 +2,7 @@
  * 其他变化
  *     for-each-in:迭代便利每一个属性并返回属性值
  *     isXMLName():接受一个字符串，检测字符串是否时元素内部名称
+ *     typeof:在对XML对象或XMLList对象使用时返回“xml”
  */
 
 var employees = <employees>
@@ -30,5 +31,7 @@ for each(var color in colors) {
     alert(color);
 }
 
-alert(isXMLName("color");    //true
+alert(isXMLName("color"));    //true
+
+alert(tyoeof xml);    //"xml"
 
