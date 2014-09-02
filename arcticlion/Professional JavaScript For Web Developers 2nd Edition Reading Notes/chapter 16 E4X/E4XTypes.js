02b548938df13d3cfb56e498d33f978c4677d29c
arcticlion <david.malan@aol.com>
2014-09-02 15:49:52 +0800

    three ways of creating a XML object and compare toSting to toXMLString in E4XTypes.js

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 16 E4X/E4XTypes.js
@@ -0,0 +1,34 @@
+/*
+ * E4X类型
+ *     XML：XML结构中任何一个独立的部分
+ *         表现XML结构中任何独立的部分
+ *             元素
+ *             特性
+ *             注释
+ *             处理指令
+ *             文本节点
+ *         最强大的是直接将XML字面量赋值给变量
+ *     XNLList：XML对象的集合
+ *     Namespace：命名空间的前缀与空间URI之间的映射
+ *     QName：内部名称和命名空间URI组成的一个限定名
+ */
+
+//创建空XML对象
+var x = new XML();
+
+//传入到XML()字符串会被解析为分层的XML对象
+var x = new XML("<employee position=\"Software Engineer\"><name>Nicholas " + "Zakas</name></employee>");
+
+//也可传入DOM文档或节点
+var x = new XML(xmldom);
+
+//XML字面量指定数据给变量
+var employee = <employee position="Software Engineer">
+                    <name>Nicholas C. Zakas</name>
+               </employee>;
+
+//toXMLString()与toString()之间的比较
+var data = <name>Nicholas C. Zakas</name>
+alert(data.toString());    //"Nicholas C. Zakas"
+alert(data.toXMLString());     //"<name>Nicholas C. Zakas</name>"
+
