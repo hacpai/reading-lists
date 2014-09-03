ffe6f65413b505dee11a2a1a19fd2e34f0b6078b
arcticlion <david.malan@aol.com>
2014-09-03 13:22:48 +0800

    check status in XHR.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/XHR.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/XHR.js
@@ -11,6 +11,12 @@
  *             表示是否异步发送请求的布尔值
  *         send():发送特定的请求
  *             接受请求主体发送的数据为参数
+ *         responseText:作为响应主体被返回的文本
+ *         responseXML:若响应的内容类型是"text/xml"或"application/xml"，这个属性将保持着响应数据的XML DOM文档
+ *         status:响应的HTTP状态
+ *             状态码200:成功的标识
+ *             状态码304:请求资源未被修改，可直接使用缓存版本
+ *         statusText;HTTP状态的说明
  */
 
 //适用于IE7之前的版本
@@ -43,4 +49,10 @@ var xhr = createXHR();
 
 xhr.open("get", "example.php", false);
 xhr.send(null);
+
+if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+    alert(xhr.statusText);
+} else {
+    alert("Request was unsuccessful: " + xhr.status);
+}
 
