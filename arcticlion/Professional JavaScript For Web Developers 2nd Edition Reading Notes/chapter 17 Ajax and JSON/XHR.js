f0c5ccfb243709006489f051b5333d075aefca34
arcticlion <david.malan@aol.com>
2014-09-03 12:51:51 +0800

    extend support to earilier version of IE in createXHR in XHR.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/XHR.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/XHR.js
@@ -6,21 +6,27 @@
 
 //适用于IE7之前的版本
 function createXHR() {
-    if (typeof arguments.callee.activeXString != "string") {
-        var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
                         "MSXML2.XMLHttp"];
 
-        for (var i = 0, len = versions.length; i < len; i++) {
-            try {
-                var xhr = new ActiveXObject(versions[i]);
-                arguments.callee.activeXString = versions[i];
-                return xhr;
-            } catch (ex) {
+            for (var i = 0, len = versions.length; i < len; i++) {
+                try {
+                    var xhr = new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    return xhr;
+                } catch (ex) {
                 //跳过
+                }
             }
         }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error("No XHR object available.");
     }
 
-    return new ActiveXObject(arguments.callee.activeXString);
 }
 
