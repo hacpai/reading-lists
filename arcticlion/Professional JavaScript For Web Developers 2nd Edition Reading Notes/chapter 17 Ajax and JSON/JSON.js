5caf09f2c79352cddbca93766fc2f9f163727c6c
arcticlion <david.malan@aol.com>
2014-09-03 21:46:34 +0800

    create object-literal and array-literal in JSON.js

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/JSON.js
@@ -0,0 +1,39 @@
+/*
+ * JSON
+ */
+
+//对象字面量表示对象
+{
+    "name": "Nicholas C. Zakas",
+    "title": "Software Engineer",
+    "author": true,
+    "age": 29
+}
+
+//可以直接赋值
+var person = {
+    "name": "Nicholas C. Zakas",
+    "title": "Software Engineer",
+    "author": true,
+    "age": 29
+};
+
+//数组字面量表示数组
+[1, 2, "color", true, null]
+
+//描述人的对象数组
+[
+    {
+        "name": "Nicholas C. Zakas",
+        "title": "Software Enginner",
+        "author": true,
+        "age": 29
+    },
+    {
+        "name": "Jim Smith", 
+        "title": "Salesperson",
+        "author": false,
+        "age": 35
+    }
+]
+
