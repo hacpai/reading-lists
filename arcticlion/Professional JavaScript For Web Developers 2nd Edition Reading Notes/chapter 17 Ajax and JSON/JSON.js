99a5f96544a35dda97316563e3ca0dd759a1082b
arcticlion <david.malan@aol.com>
2014-09-03 22:03:11 +0800

    write a Usage of parse in JSON.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/JSON.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 17 Ajax and JSON/JSON.js
@@ -1,5 +1,11 @@
 /*
  * JSON
+ *     Ajax使用JSON
+ *         parse():2个参数,与eval()不同的时不需要传入圆括号
+ *             JSON文本
+ *             可选的过滤函数
+ *                 JSON键和值为参数
+ *                 返回值为结果对象关联的值，为重写默认的解析机制提供机会
  */
 
 //对象字面量表示对象
@@ -50,4 +56,18 @@ if (people[0].author) {
 
 //将输入的文本放在圆括号中，防止出错
 var object = eval("(" + jsonText + ")");
+
+var object = JSON.parse("{}");
+
+var jsonText = "{\"name\":\"Nicholas C. Zakas\", \"age\":29, \"author\":true }";
+var object = JSON.parse(jsonText, function(key, value) {
+    switch(key) {
+        case "age": return value + 1;
+        case "author": return undefined;
+        default: return value;
+    }
+});
+
+alert(object.age);    //30
+alert(object.author);    //undefined
 
