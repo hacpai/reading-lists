98e1e3bd44a4e1ad47be62ee1b33b9ad88286e81
arcticlion <david.malan@aol.com>
2014-09-04 19:48:35 +0800

    write a Function of Person in advancedFunctions.js

---
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/advancedFunctions.js
@@ -0,0 +1,14 @@
+/*
+ * 高级函数
+ *    作用域安全的构造函数
+ */
+
+function Person(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+}
+
+var person = new Person("Nicholas", 29, "Software Engineer");
+alert(person.name);    //"Nicholas"
+
