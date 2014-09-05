5f042b3b35a6f71b17a5772d8d3985c10edd674d
arcticlion <david.malan@aol.com>
2014-09-05 13:28:46 +0800

    chains call to setTimeout() in advancedTimers.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/advancedTimers.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/advancedTimers.js
@@ -3,6 +3,13 @@
  *     指定的时间间隔表示何时将定时器的代码添加到队列 
  *     浏览器再根据进程空闲依次执行队列内的代码
  *     故一般执行时间大于设置的时间
+ *     重复的定时器
+ *         浏览器仅当没有该定时器的代码实例时才将定时器代码添加到队列中导致2个问题
+ *             某些间隔被跳过
+ *             多个定时器的代码执行之间的间隔可能比预期的小
+ *         避免setInterval()重复2个错误
+ *             链式setTimeout调用
+ *         
  */
 
 var btn = document.getElementById("my-btn");
@@ -24,4 +31,10 @@ setTimeout(function(diff) {
         //调用及时
     }
 }, 250);
+
+//链式调用setTimeout()，每次函数执行都创建一个新的定时器
+setTimeout(function() {
+    //处理中
+    setTimeout(arguments.callee, interval);
+}, interval);
 
