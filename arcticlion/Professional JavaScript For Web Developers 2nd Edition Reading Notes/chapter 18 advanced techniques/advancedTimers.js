1eb72bab867445259ee2d22643b949f64b4726ae
arcticlion <david.malan@aol.com>
2014-09-05 14:22:55 +0800

    write a array chunking pattern in advancedTimers.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/advancedTimers.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/advancedTimers.js
@@ -9,6 +9,10 @@
  *             多个定时器的代码执行之间的间隔可能比预期的小
  *         避免setInterval()重复2个错误
  *             链式setTimeout调用
+ *     Yielding Processes
+ *         长时间运行循环会降低用户的交互体验
+ *         对于无须同步完成和无须顺序完成的数据
+ *             使用数组分块技术：小块小块地处理数组
  *         
  */
 
@@ -47,4 +51,21 @@ setTimeout(function() {
         setTimeout(arguments.callee, 50);
     } 
 }, 50);
+
+//数组分块技术
+    //array蕾丝“待办事宜”列表，包含要处理的项目
+    //shift()获取队列中下一个要处理的项目
+    //然后传递给函数
+    //如果队列中还有其他项目
+    //则设置另一个定时器通过arguments.callee调用同一个匿名函数
+setTimeout(function(){
+    //取出下一个条目并处理
+    var item = array.shift();
+    process(item);
+
+    //若还有条目，再设置另一个定时器
+    if (array.length > 0) {
+        setTimeout(arguments.callee, 100);
+    }
+}, 100);
 
