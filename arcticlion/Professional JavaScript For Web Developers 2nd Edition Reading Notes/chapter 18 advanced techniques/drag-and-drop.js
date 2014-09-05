ce2e19951601cad775929a5dbd849811ec4f52a5
arcticlion <david.malan@aol.com>
2014-09-05 23:21:44 +0800

    add custom event in drag-and-drop.js

--- arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/drag-and-drop.js
+++ arcticlion/Professional JavaScript For Web Developers 2nd Edition Reading Notes/chapter 18 advanced techniques/drag-and-drop.js
@@ -2,6 +2,9 @@
  * 拖放
  *     设置onmousemove事件处理程序，总是将指定元素移动到鼠标指针位置上
  *     修缮拖动功能
+ *     添加自定义事件
+ *         让拖动事件真正应用起来
+ *         定义3个事件：dragstart、drag、dragend
  */
 
 EventUtil.addHandler(document, "mousemove", function(event) {
@@ -10,8 +13,10 @@ EventUtil.addHandler(document, "mousemove", function(event) {
     myDiv.style.top = event.clientY + "px";
 });
 
-//最简单的拖放界面
+//最简单的拖放界面(加强版)
 var DragDrop = function() {
+
+    var dragdrop = new EventTarget();
     //存放被拖动的元素
     var dargging = null;
     //保存x和y坐标的差值
@@ -33,6 +38,8 @@ var DragDrop = function() {
                     dragging = target;
                     diffX = event.clientX - target.offsetLeft;
                     diffY = event.clientY - target.offsetTop;
+                    dragdrop.fire( {type:"dragstart", target: dragging,
+                                   x: event.clientX, y: event.clientY});
                 }
                 break;
             
@@ -45,32 +52,52 @@ var DragDrop = function() {
                     //指定位置
                     dragging.style.left = (event.clientX - diffX) + "px";
                     dragging.style.top = (event.clientY - diffY) + "px";
+
+                    //触发自定义事件
+                    dragdrop.fire({type:"drag", target: dragging,
+                                   x: event.clientX, y: event.clientY});
                 }
                 break;
 
             case "mouseup":
+                dragdrop.fire({type: "dragend", target: dragging,
+                               x: event.clientX, y: event.clientY});
                 dragging = null;
                 break;     
         }
     };
 
     //公共接口
-    return {
-        enable: function() {
+        dragdrop.enable: function() {
             EventUtil.addHandler(document, "mousedown", handleEvent);
             EventUtil.addHandler(document, "mousemove", handleEvent);
             EventUtil.addHandler(document, "mouseup", handleEvent);
         },
 
-        disable: function() {
+        dragdrop.disable: function() {
             EventUtil.removeHandler(document, "mousedown", handleEvent);
             EventUtil.removeHandler(document, "mousemove", handleEvent);
             EventUtil.removeHandler(document, "mouseup", handleEvent);
         }
-    }
+    return dragdrop; 
 }();
 
 //针对所有包含"draggable"类的元素启用
 //为了元素能够被托 dog，必须绝对定位
 <div class="draggable" style="position:absolute; background:red" ></div>
+
+DragDrop.addHandler("drafstart", function(event) {
+    var status = document.getElementById("status");
+    status.innerHTML = "Started dragging " + event.target.id;
+});
+
+DragDrop.addHandler("drag", function(event) {
+    var status = document.getElementById("status");
+    status.innerHTML += " <br/>Dragged" + event.target.id + " to (" + event.x + "," + event.y + ")";
+});
+
+DragDrop.addHandler("dragend", function(event) {
+    var status = document.getElementById("status");
+    status.innerHTML += " <br/>Dropped " + event.target.id + " at (" + event.x + "," + event.y + ")";
+});
 
