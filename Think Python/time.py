a29f5408a7260905587163e838daea157f7fdd2f
arcticlion <david.malan@aol.com>
2015-02-18 11:40:55 +0800

    Refine time.py

--- Think Python/time.py
+++ Think Python/time.py
@@ -84,4 +84,18 @@ class Time( object ):
         return minute * 60 + self.second
 
 
+class Time( object ):
+    """Represents a time of whole day.
 
+    attributes: seconds
+    """
+    def __init__( self, hour, minute, second ):
+        minutes = hour * 60 + minute
+        self.seconds = minutes * 60 + second
+
+    def __comp__( self, other ):
+        """Returns a positive number if the first object is greater,
+        a negative number if the second object is greater,
+        and 0 if they are equal to each other.
+        """
+        return cmp( self.seconds, other.seconds )
